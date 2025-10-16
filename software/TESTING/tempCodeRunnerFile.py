e2f0a83c5e65bdbe033ca376cb484b2f7aefa32f
Martin Matvoz <matvoz.martin@gmail.com>
2025-10-16 11:24:15 +0200

    Removed unneeded testing scripts

--- software/TESTING/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-while True:
-    #     data = ser.read(1)
-    #     if not data:
-    #         continue
-    #     if data == b"\r":
-    #         print()
-    #     else:
-    #         print(data.decode(errors="replace"), end="")
